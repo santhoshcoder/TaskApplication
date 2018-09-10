12589db0d4a00bfa2514bb6f9c0241c544243f98
santhoshcoder <santhoshgodishala@yahoo.com>
2018-09-10 14:27:36 -0500

    GUI Setup for Categories

--- tasker.java
+++ tasker.java
@@ -19,11 +19,14 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class tasker {
 
 	private JFrame frame;
 	int p2row,p2col;
+	private JTextField textField;
 	/**
 	 * Launch the application.
 	 */
@@ -91,55 +94,31 @@ public class tasker {
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 432, 41);
 		frame.getContentPane().add(panel);
-		panel.setLayout(new MigLayout("", "[]", "[]"));
+		panel.setLayout(new MigLayout("", "[][][][][][]", "[]"));
 		
 		JLabel lblNotes = new JLabel("Tasker");
 		lblNotes.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		panel.add(lblNotes, "cell 0 0,growx");
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(110, 41, 322, 212);
-		frame.getContentPane().add(panel_2);
-		panel_2.setLayout(new MigLayout("", "[]", "[]"));
+		panel.add(lblNotes, "cell 5 0,growx");
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 41, 110, 212);
+		panel_1.setBounds(0, 41, 185, 212);
 		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(new MigLayout("", "[]", "[][]"));
+		panel_1.setLayout(new MigLayout("", "[grow]", "[][]"));
 		
-		JButton btnFirst = new JButton("First");
-		btnFirst.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				panel_2.removeAll();
-				p2row = 0;
-				p2col = 0;
-				JButton b1 = new JButton("First");
-				JButton b2 = new JButton("Second");
-				panel_2.add(b1,"cell "+p2row+" "+p2col+",growx");
-				p2col++;
-				panel_2.add(b2,"cell "+p2row+" "+p2col+",growx");
-				panel_2.revalidate();
-				panel_2.repaint();
-			}
-		});
+		JLabel lblCategories = new JLabel("Categories");
+		lblCategories.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_1.add(lblCategories, "cell 0 0");
 		
+		textField = new JTextField();
+		panel_1.add(textField, "flowx,cell 0 1,growx");
+		textField.setColumns(10);
 		
-		panel_1.add(btnFirst, "cell 0 0,growx");
-		JButton btnSecond = new JButton("Second");
-		btnSecond.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				panel_2.removeAll();
-				p2row = 0;
-				p2col = 0;
-				JButton b1 = new JButton("Third");
-				JButton b2 = new JButton("Fourth");
-				panel_2.add(b1,"cell "+p2row+" "+p2col+",growx");
-				p2col++;
-				panel_2.add(b2,"cell "+p2row+" "+p2col+",growx");
-				panel_2.revalidate();
-				panel_2.repaint();
-			}
-		});
-		panel_1.add(btnSecond, "cell 0 1,growx");
+		JButton button = new JButton("+");
+		panel_1.add(button, "cell 0 1");
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(183, 41, 249, 212);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(new MigLayout("", "[]", "[]"));
 	}
 }

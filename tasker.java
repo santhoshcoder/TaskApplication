711747f887196d7287670d16588dd986251e73e4
santhoshcoder <santhoshgodishala@yahoo.com>
2018-09-10 20:23:47 -0500

    Category Addition and Validation

--- tasker.java
+++ tasker.java
@@ -86,7 +86,7 @@ public class tasker {
 			   String password = "";
 			   Class.forName(driver);
 			   Connection conn = DriverManager.getConnection(url,username,password);
-			   System.out.println("Connected");
+			   //System.out.println("Connected");
 			   return conn;
 		  } 
 		  catch(Exception e)
@@ -127,6 +127,50 @@ public class tasker {
 		textField.setColumns(10);
 		
 		JButton button = new JButton("+");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String catName = textField.getText();
+				catName = catName.trim();
+				if(catName.length() != 0){
+					catName = catName.toLowerCase();
+					//System.out.println("Valid Category Name:"+catName);
+					//Check if the category already exists
+					try {
+						Connection con = getConnection();
+						PreparedStatement pr = con.prepareStatement("SELECT * FROM initial WHERE cat = ?");
+						pr.setString(1,catName);
+						ResultSet rs = pr.executeQuery();
+						if(rs.next()) {
+							System.out.println("Category Already Exists");
+						}
+						else {
+							pr = con.prepareStatement("INSERT INTO initial values(?,?,?)");
+							Statement s = con.createStatement();
+							String sql = "INSERT INTO initial values("+null+","+"'"+catName+"'"+","+null+")";
+							s.execute(sql);
+							System.out.println("Query Executed");
+							
+							String cap_column_name = catName.substring(0, 1).toUpperCase() + catName.substring(1).toLowerCase();
+							JButton j = new JButton(cap_column_name);
+							categoryListener cl = new categoryListener();
+							j.addActionListener(cl);
+							panel_1.add(j,"cell "+panel_1col+" "+panel_1row+",growx");
+							panel_1row++;
+							
+							panel_1.revalidate();
+							panel_1.repaint();
+						}
+					}
+					catch(Exception e) {
+						e.printStackTrace();
+					}
+				}
+				else {
+					System.out.println("Invalid Category Name:"+"*"+catName+"*");
+				}
+				textField.setText("");
+			}
+		});
 		panel_1.add(button, "cell 0 1");
 		
 		//JPanel panel_2 = new JPanel();
@@ -146,6 +190,7 @@ public class tasker {
 			String text = b.getText();
 			selected_cat = text.substring(0, 1).toLowerCase() + text.substring(1);
 			resetPanel2();
+			System.out.println("Cateogry Selected is:"+selected_cat);
 			addPanel2Elements();
 		}
 		
@@ -180,15 +225,17 @@ public class tasker {
 			//System.out.println("Selected Category is:"+selected_cat);
 			//Select data from the database
 			Connection con = getConnection();
-			PreparedStatement pr = con.prepareStatement("SELECT task FROM initial WHERE cat = ?");
+			PreparedStatement pr = con.prepareStatement("SELECT task FROM initial WHERE cat = ? AND task IS NOT NULL");
 			pr.setString(1,selected_cat);
 			ResultSet rs = pr.executeQuery();
 			while(rs.next()) {
 				String name = rs.getString("task");
+				System.out.println("Task Name is:"+name);	
 				String task_name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
 				JCheckBox j1 = new JCheckBox(task_name);
 				panel_2.add(j1,"cell "+panel_2col+" "+panel_2row+",growx");
 				panel_2row++;
+			
 			}
 			panel_2.revalidate();
 			panel_2.repaint();
